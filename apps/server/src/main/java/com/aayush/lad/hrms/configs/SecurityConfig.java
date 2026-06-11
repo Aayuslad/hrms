e0e56d54a1af2a9a471b28e2a0b1cfa06cc11932
AayushLad <aayush.lad@roimaint.com>
2026-06-11 19:23:48 +0530

    feat: add Spring Boot Actuator dependency and configure health endpoint; disable SpringDoc API docs and Swagger UI in production

--- apps/server/src/main/java/com/aayush/lad/hrms/configs/SecurityConfig.java
+++ apps/server/src/main/java/com/aayush/lad/hrms/configs/SecurityConfig.java
@@ -35,7 +35,8 @@ public class SecurityConfig {
                                 "/api/users/register",
                                 "/v3/api-docs/**",
                                 "/swagger-ui/**",
-                                "/swagger-ui.html"
+                                "/swagger-ui.html",
+                                "/actuator/health"
                         ).permitAll()
                         .anyRequest().authenticated()
                 )
