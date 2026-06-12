e2d588eae44e979cc79e4c20912c5f8545fc140f
AayushLad <aayush.lad@roimaint.com>
2026-06-12 12:01:29 +0530

    fix: update axios baseURL to use the correct production server

--- apps/client/src/lib/axios-client.ts
+++ apps/client/src/lib/axios-client.ts
@@ -1,8 +1,8 @@
 import axios from 'axios';
 
 export const axiosClient = axios.create({
-    // baseURL: 'https://server.hrms.aayushlad.online/api',
-    baseURL: 'https://hrms-server-8bba.onrender.com/api',
+    baseURL: 'https://server.hrms.aayushlad.online/api',
+    // baseURL: 'https://hrms-server-8bba.onrender.com/api',
     // baseURL: 'http://localhost:8080/api',
     withCredentials: true,
     headers: {
